a97a0403277b46cb2e18917cddb39e82b6286af5
jacob-s-codes <jacobbshaul@gmail.com>
2024-11-25 12:56:56 -0800

    updated

--- app/components/LandingPage.tsx
+++ app/components/LandingPage.tsx
@@ -6,7 +6,7 @@ const LandingPage = () => {
         <div className='text-primary flex flex-row items-center justify-evenly mt-40'>
             <div className='flex flex-col items-start gap-y-24'>
                 <h1 className='text-8xl'>Jacob Shaul</h1>
-                <h2 className='max-w-xl text-2xl'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nulla alias error consequuntur itaque laboriosam recusandae doloribus quam excepturi quo nostrum.</h2>
+                <h2 className='max-w-xl text-2xl'>, ipsum dolor sit amet consectetur adipisicing elit. Nulla alias error consequuntur itaque laboriosam recusandae doloribus quam excepturi quo nostrum.</h2>
             </div>
             <Image src="/facepicture.jpg" className='w-72 h-full rounded-3xl' width={500} height={500}  alt='Different picture'/>
 

fabb81820378e4acf30d061d3a7fdcace135d67f
jacob-s-codes <jacobbshaul@gmail.com>
2024-12-07 15:58:33 -0800

    deleted the svg's, see if anything breaks, image still not working

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/personal-portfolio', // Replace 'repository-name' with your GitHub repository name
   images: {
-    unoptimized: true, // Needed for static hosting like GitHub Pages
-  },
+    formats: ['image/webp', 'image/avif']
+  }
 };
 
 export default nextConfig;
-

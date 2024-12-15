69ef1988995eed466d9413aea0df6d51a938b546
jacob-s-codes <jacobbshaul@gmail.com>
2024-12-15 15:42:11 -0800

    see if this adds the new thing

--- app/components/MyProjects.tsx
+++ app/components/MyProjects.tsx
@@ -12,7 +12,7 @@ const MyProjects = () => {
                     <hr className='mx-8 mt-2 h-1 bg-white border-0'/>
                 </SpotlightBorder>
             </div>
-            <div className='flex flex-row  items-center justify-center mt-96 z-0'>
+            <div className='flex flex-row items-center justify-center mt-96 z-0'>
                 <SpotlightBorder className="h-[600px] w-96 flex flex-row items-center justify-center" spotlightColor="#0EA5E9">
                     <h1>Text</h1>
                 </SpotlightBorder>

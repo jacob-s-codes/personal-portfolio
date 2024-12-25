04452a695ea0113bd2fbb4d74a2c76bae7e09a54
jacob-s-codes <jacobbshaul@gmail.com>
2024-12-24 21:37:25 -0800

    added second mode to code img

--- app/components/MyProjects.tsx
+++ app/components/MyProjects.tsx
@@ -24,8 +24,20 @@ const MyProjects = () => {
                 </SpotlightBorder>
             </div>
             <div className='flex flex-row items-center justify-center mt-96 z-0'>
-                <SpotlightBorder className="h-[600px] w-96 flex flex-row items-center justify-center" spotlightColor="#0EA5E9">
-                    <h1>Text</h1>
+                <SpotlightBorder className="h-[600px] w-96 hover:scale-[105%] duration-1000 ease-in-out bg-black" spotlightColor="white">
+                    <div className='pl-8 pt-8'>
+                        <h2 className='text-3xl'>Mode to Code</h2>
+                    </div>
+                    
+                    
+                    
+                    <hr className='mx-8 mt-2 h-1 bg-white border-0'/>
+                    <div className='px-8 h-auto'>
+                        <Image src={modetocode} alt="image of mode to code" className='rounded-lg'></Image>
+                    </div>
+                    <div className='px-8 py-8'>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, repudiandae tenetur necessitatibus esse consequuntur assumenda aspernatur ab, officiis impedit voluptates fuga excepturi, asperiores nemo omnis repellat iste. Nisi, dolor ullam.</p>
+                    </div>
                 </SpotlightBorder>
             </div>
         </div>

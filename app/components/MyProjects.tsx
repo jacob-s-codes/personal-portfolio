c91b46ac3c2311e59dcaa7e3296f2a6c752b5eb3
jacob-s-codes <jacobbshaul@gmail.com>
2024-12-29 21:56:20 -1000

    updated second project

--- app/components/MyProjects.tsx
+++ app/components/MyProjects.tsx
@@ -7,7 +7,7 @@ const MyProjects = () => {
     return (
         <div className='flex flex-row items-center justify-center gap-36'>
             <div className='flex flex-row items-center justify-center mt-96 z-0'>
-                <SpotlightBorder className="h-[650px] w-96 duration-500 ease-in-out bg-[#050126] hover:scale-105" spotlightColor="white">
+                <SpotlightBorder className="h-[650px] w-96 duration-500  bg-[#050126] hover:scale-105 hover:shadow-xl hover:shadow-[#D0E4E3] ease-in" spotlightColor="white">
                     <div className='pl-8 pt-8'>
                         <h2 className='text-3xl'>Mode to Code</h2>
                     </div>
@@ -27,19 +27,22 @@ const MyProjects = () => {
                 </SpotlightBorder>
             </div>
             <div className='flex flex-row items-center justify-center mt-96 z-0'>
-                <SpotlightBorder className="h-[600px] w-96 duration-1000 ease-in-out bg-black" spotlightColor="white">
+            <SpotlightBorder className="h-[650px] w-96 duration-500  bg-[#050126] hover:scale-105 hover:shadow-2xl hover:shadow-white ease-in" spotlightColor="white">
                     <div className='pl-8 pt-8'>
-                        <h2 className='text-3xl'>Mode to Code</h2>
+                        <h2 className='text-3xl'>School Projects</h2>
                     </div>
                     
                     
                     
-                    <hr className='mx-8 mt-2 h-1 bg-white border-0'/>
-                    <div className='px-8 h-auto'>
-                        <Image src={modetocode} alt="image of mode to code" className='rounded-lg'></Image>
+                    <hr className='mx-8 mt-1 h-1 bg-white border-0'/>
+                    <div className='px-8 h-auto mt-8'>
+                        <Image src={modetocode} alt="image of mode to code" className='rounded-lg border-white border-2'></Image>
                     </div>
                     <div className='px-8 py-8'>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, repudiandae tenetur necessitatibus esse consequuntur assumenda aspernatur ab, officiis impedit voluptates fuga excepturi, asperiores nemo omnis repellat iste. Nisi, dolor ullam.</p>
+                        <p>This was a project that helps teach students how to code and other basic software technologies.</p>
+                    </div>
+                    <div className='flex w-full justify-center'>
+                        <button className='border-white border-2 rounded-lg px-4 py-4 hover:bg-white hover:text-black duration-500 hover:cursor-pointer'>Learn more</button>
                     </div>
                 </SpotlightBorder>
             </div>

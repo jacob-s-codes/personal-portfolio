6e46ab9ff0b4e1494426cc9537dd1b84cad72a77
jacob-s-codes <jacobbshaul@gmail.com>
2025-01-01 11:14:25 -1000

    broke a bunch of stuff, try to fix it later

--- app/components/MyProjects.tsx
+++ app/components/MyProjects.tsx
@@ -3,6 +3,7 @@ import { SpotlightBorder } from './SpotlightBorder'
 import Image from 'next/image'
 import modetocode from '../assets/modetocodebg.png'
 import schoolimg from '../assets/ifbackground.jpeg'
+import Link from 'next/link'
 
 const MyProjects = () => {
     return (
@@ -10,7 +11,7 @@ const MyProjects = () => {
             <h3 className='flex justify-center text-5xl text-center mb-0 mt-96 font-bold'>What I've been working on:</h3>
             <div className='flex flex-row items-center justify-center gap-36'>
                 <div className='flex flex-row items-center justify-center mt-24 z-0'>
-                    <SpotlightBorder className="h-[650px] w-96 duration-500  bg-[#050126] hover:scale-105 hover:shadow-xl hover:shadow-[#D0E4E3] ease-in" spotlightColor="white">
+                    <SpotlightBorder className="h-[650px] w-96 duration-500  bg-[#050126] hover:shadow-xl hover:shadow-[#D0E4E3]" spotlightColor="white">
                         <div className='pl-8 pt-8'>
                             <h2 className='text-3xl'>Mode to Code</h2>
                         </div>
@@ -25,12 +26,12 @@ const MyProjects = () => {
                             <p>This was a project that helps teach students how to code and other basic software technologies.</p>
                         </div>
                         <div className='flex w-full justify-center'>
-                            <button className='border-white border-2 rounded-lg px-4 py-4 hover:bg-white hover:text-black duration-500 hover:cursor-pointer'>Learn more</button>
+                        <Link href="/modetocode" target='_blank'><button className='border-white border-2 rounded-lg px-4 py-4 hover:bg-white hover:text-black duration-500 hover:cursor-pointer'>Learn more</button></Link>
                         </div>
                     </SpotlightBorder>
                 </div>
                 <div className='flex flex-row items-center justify-center mt-24 z-0'>
-                <SpotlightBorder className="h-[650px] w-96 duration-500  bg-[#667a8a] hover:scale-105 hover:shadow-2xl hover:shadow-white ease-in-out" spotlightColor="white">
+                <SpotlightBorder className="h-[650px] w-96 duration-500  bg-[#667a8a] hover:shadow-xl hover:shadow-white " spotlightColor="white">
                         <div className='pl-8 pt-8'>
                             <h2 className='text-3xl'>School Projects</h2>
                         </div>
@@ -45,7 +46,7 @@ const MyProjects = () => {
                             <p>Here are some projects that I worked on during high school for some of my classes. This project was a lot of fun to work on and I enjoyed it.</p>
                         </div>
                         <div className='flex w-full justify-center'>
-                            <button className='border-white border-2 rounded-lg px-4 py-4 hover:bg-white hover:text-black duration-500 hover:cursor-pointer'>Learn more</button>
+                            <Link href="/modetocode"><button className='border-white border-2 rounded-lg px-4 py-4 hover:bg-white hover:text-black duration-500 hover:cursor-pointer'>Learn more</button></Link>
                         </div>
                     </SpotlightBorder>
                 </div>
---
+++ app/modetocode/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+const page = () => {
+  return (
+    <div className='bg-black text-white'>
+        <NavBar/>
+    </div>
+  )
+}
+
+export default page

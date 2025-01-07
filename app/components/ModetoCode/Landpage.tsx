b5ccb9eec17e0a133f105b0c0073723d6129a30a
jacob-s-codes <jacobbshaul@gmail.com>
2025-01-06 18:24:38 -0800

    just putting in the images regularly

--- app/components/ModetoCode/Landpage.tsx
+++ app/components/ModetoCode/Landpage.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import Image from 'next/image';
-import modetocode from '../../assets/modetocodebg.png'
+import modetocode from '../../assets/mtcforpage.png'
 
 const Landpage = () => {
   return (
-    <div className=''>
-        <Image src={modetocode} alt="mode to code image" width={500} height={500} className='w-[50%] h-auto top-0'/>
+    <div className='bg-[#050126] py-[30vh]'>
+        <div className='flex flex-row items-center justify-center'>
+            <Image src={modetocode} alt="mode to code image" width={500} height={500} className='w-[50%] h-auto '/>
+            <div>
+                
+            </div>
+        </div>
+
     </div>
   )
 }

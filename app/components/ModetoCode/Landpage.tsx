bad29cdf76914ad3fbb5139febbf57b01096d2ad
jacob-s-codes <jacobbshaul@gmail.com>
2025-01-08 21:16:24 -0800

    fixed weird bugs

--- app/components/ModetoCode/Landpage.tsx
+++ app/components/ModetoCode/Landpage.tsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import Image from 'next/image';
-import modetocode from '../../assets/mtcforpage.png'
+
 
 const Landpage = () => {
-  return (
-    <div className='bg-[#050126] py-[30vh]'>
-        <div className='flex flex-row items-center justify-center'>
-            <Image src={modetocode} alt="mode to code image" width={500} height={500} className='w-[50%] h-auto '/>
-            <div>
-                
-            </div>
-        </div>
+    return (
+        <div className='bg-[#050126] py-[30vh]'>
+            <div className='flex flex-row items-center justify-center'>
+                {/* <video src="../mtcvideo.mp4" autoPlay
+                    loop
+                    muted
+                    playsInline
+                    style={{ filter: 'brightness(40%)' }}
+                    className=" w-full h-auto max-w-[1800px] object-cover"></video> */}
+                {/* <Image src={modetocode} alt="mode to code image" width={500} height={500} className='w-[50%] h-auto ' />
+                <div>
 
-    </div>
-  )
+                </div> */}
+            </div>
+
+        </div>
+    )
 }
 
 export default Landpage

4e974a8a46e54cadc93527d29de94ab975a57fe1
jacob-s-codes <jacobbshaul@gmail.com>
2025-01-11 12:10:29 -0800

    see if this fixes build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Orbitron } from "next/font/google";
-import localFont from "next/font/local";
+// import localFont from "next/font/local";
 import "./globals.css";
 
 
--- app/modetocode/page.tsx
+++ app/modetocode/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBar from '../components/HomePage/NavBar';
-import { Orbitron } from 'next/font/google';
+// import { Orbitron } from 'next/font/google';
 import Landpage from '../components/ModetoCode/Landpage';
 
 // const orbitron = Orbitron({

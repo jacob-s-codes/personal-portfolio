fcd54e33a41dcd332bf4fcff186e3b526142fe13
jacob-s-codes <jacobbshaul@gmail.com>
2025-01-15 20:51:15 -0800

    rounded image

--- app/components/schoolprojects/Landpage.tsx
+++ app/components/schoolprojects/Landpage.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+import englishImage from '../../assets/ifbackground.jpeg';
 
 const Landpage = () => {
   return (
@@ -6,10 +8,13 @@ const Landpage = () => {
       <h1 className='text-9xl text-center mt-8'>School Projects</h1>
 
 
-      <div className='flex flex-row items-center justify-center'>
-        <div>
+      <div className='flex flex-row items-center justify-center mt-56 gap-x-12'>
+        <div className='flex flex-col max-w-2xl gap-y-12 ml-24'>
           <h2 className='text-5xl'>English Final Project</h2>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus et consequatur rem maxime, reiciendis quisquam ab libero earum excepturi aspernatur, sit accusantium eos eveniet amet enim quasi molestias quas alias!</p>
+          <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus et consequatur rem maxime, reiciendis quisquam ab libero earum excepturi aspernatur, sit accusantium eos eveniet amet enim quasi molestias quas alias!</p>
+        </div>
+        <div className='w-full mr-24'>
+          <Image src={englishImage} width={500} height={500} className='w-full rounded-md' alt="image of english picture"></Image>
         </div>
       </div>
     </div>

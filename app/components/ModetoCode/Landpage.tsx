4286827492d7270e13f2f36ba1a67edc621fb191
jacob-s-codes <jacobbshaul@gmail.com>
2025-01-17 23:10:27 -0800

    updated mtc home

--- app/components/ModetoCode/Landpage.tsx
+++ app/components/ModetoCode/Landpage.tsx
@@ -4,19 +4,8 @@ import React from 'react';
 
 const Landpage = () => {
     return (
-        <div className='bg-[#050126] py-[30vh]'>
-            <div className='flex flex-row items-center justify-center'>
-                {/* <video src="../mtcvideo.mp4" autoPlay
-                    loop
-                    muted
-                    playsInline
-                    style={{ filter: 'brightness(40%)' }}
-                    className=" w-full h-auto max-w-[1800px] object-cover"></video> */}
-                {/* <Image src={modetocode} alt="mode to code image" width={500} height={500} className='w-[50%] h-auto ' />
-                <div>
-
-                </div> */}
-            </div>
+        <div className='bg-[#050126] top-0'>
+            <h1 className='text-9xl text-center py-56 '>Mode to Code</h1>
 
         </div>
     )

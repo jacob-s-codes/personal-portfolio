11b1291b4934cf812e8841192ed7ff52665d739b
jacob-s-codes <jacobbshaul@gmail.com>
2025-01-18 23:30:21 -0800

    fixed orientation

--- app/components/schoolprojects/Landpage.tsx
+++ app/components/schoolprojects/Landpage.tsx
@@ -19,13 +19,14 @@ const Landpage = () => {
       </div>
 
       <div className='flex flex-row items-center justify-center mt-56 gap-x-12'>
-        <div className='flex flex-col max-w-2xl gap-y-12 ml-24'>
+        <div className='w-full ml-24'>
+          <Image src={englishImage} width={500} height={500} className='w-full rounded-md shadow-xl shadow-white' alt="image of english picture"></Image>
+        </div>
+        <div className='flex flex-col max-w-2xl gap-y-12 mr-24 text-right'>
           <h2 className='text-5xl'>English Final Project</h2>
           <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus et consequatur rem maxime, reiciendis quisquam ab libero earum excepturi aspernatur, sit accusantium eos eveniet amet enim quasi molestias quas alias!</p>
         </div>
-        <div className='w-full mr-24'>
-          <Image src={englishImage} width={500} height={500} className='w-full rounded-md shadow-xl shadow-white' alt="image of english picture"></Image>
-        </div>
+
       </div>
     </div>
   )

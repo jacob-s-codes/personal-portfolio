fba86e1789ce905be88d0a1371ab739769d6e190
jacob-s-codes <jacobbshaul@gmail.com>
2025-01-29 22:48:25 -0800

    added heading for mtc

--- app/components/ModetoCode/Landpage.tsx
+++ app/components/ModetoCode/Landpage.tsx
@@ -5,7 +5,12 @@ import React from 'react';
 const Landpage = () => {
     return (
         <div className='bg-[#050126] top-0'>
-            <h1 className='text-9xl text-center py-56 '>Mode to Code</h1>
+            <div className='py-56'>
+
+                <h1 className='text-9xl text-center '>Mode to Code</h1>
+                <h2 className='text-center text-5xl pt-8'>Teaching students the vital technology that they need</h2>
+            </div>
+
             
 
         </div>

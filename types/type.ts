5d8210841371b5bd145d94fc230590dd79b98fe3
jacob-s-codes <jacobbshaul@gmail.com>
2025-03-23 20:03:12 -0700

    removed files

--- types/type.ts
+++
@@ -1,6 +0,0 @@
-export interface SpotlightProps {
-    className?: string; // Optional class names for styling
-    children: React.ReactNode; // React elements to render inside the border
-    spotlightColor: string; // The color of the spotlight effect (e.g., "rgba(255, 0, 0, 0.5)")
-  }
-  
